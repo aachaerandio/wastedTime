0b7f4c805ef5d1b74d599f9067f482498217d2b1
Araceli Achaerandio <araceli.achearandio@gmail.com>
2014-04-01 19:21:41 +0100

    Funcionando primera version del cronometro

--- WastedTime/src/com/aachaerandio/wastedtime/MainActivity.java
+++ WastedTime/src/com/aachaerandio/wastedtime/MainActivity.java
@@ -1,44 +1,89 @@
 package com.aachaerandio.wastedtime;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	private static final Boolean ON = true;
 	private static final Boolean OFF = false;
 	
 	private Boolean flag = OFF;
-	
+	private Chronometer chrono;
+	private String state = "inactive";
+	Long memoChrono;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		ImageButton button = (ImageButton) findViewById(R.id.redButton);
-		button.setOnClickListener(new View.OnClickListener() {
+		ImageButton btnRed = (ImageButton) findViewById(R.id.redButton);
+		Button resetButton = (Button) findViewById(R.id.btn_stop);
+		chrono = (Chronometer) findViewById(R.id.chronometer);
+		
+		btnRed.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				TextView label =   (TextView) findViewById(R.id.wastedTimeLabel);
-				if (flag == OFF){
-					flag = ON;
-					label.setVisibility(View.VISIBLE);
+//				if (flag == OFF){
+//					flag = ON;
+//					label.setVisibility(View.VISIBLE);					
+//				}
+//				else{
+//					flag = OFF;
+//					label.setVisibility(View.INVISIBLE);
+//				}
+				
+				if (state == "inactive") {
+					chrono.setBase(SystemClock.elapsedRealtime());
+					chrono.start();
+					state = "active";
+					return;
 				}
-				else{
-					flag = OFF;
-					label.setVisibility(View.INVISIBLE);
+				if (state == "active") {
+					memoChrono = SystemClock.elapsedRealtime();
+					chrono.stop();
+					state = "paused";
+					timeElapsed();
+					return;
+				} else {
+					chrono.setBase(chrono.getBase() + SystemClock.elapsedRealtime() - memoChrono);
+					chrono.start();
+					state = "active";
 				}
 				
+				
+			}
+		});
+		
+		resetButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				chrono.stop();
+				chrono.setBase(SystemClock.elapsedRealtime());
+				state = "inactive";				
 			}
 		});
 	}
 
+	private void timeElapsed () {
+		long timeElapsed = SystemClock.elapsedRealtime() - chrono.getBase();
+		int hours = (int) (timeElapsed / 3600000);
+		int minutes = (int) (timeElapsed - hours * 3600000) / 60000;
+		int seconds = (int) (timeElapsed - hours * 3600000 - minutes * 60000) / 1000;
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
