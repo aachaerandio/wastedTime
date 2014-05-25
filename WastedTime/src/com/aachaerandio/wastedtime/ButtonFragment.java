5616bdfc91579e8563fe3c5affea026fb9a23be4
Araceli Achaerandio <araceli.achearandio@gmail.com>
2014-05-25 21:05:01 +0100

    Refactor del código

--- WastedTime/src/com/aachaerandio/wastedtime/ButtonFragment.java
+++ WastedTime/src/com/aachaerandio/wastedtime/ButtonFragment.java
@@ -28,8 +28,7 @@ public class ButtonFragment extends Fragment {
 	private ImageButton redButton;
 	
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 		rootView = inflater.inflate(R.layout.fragment_button, container, false);
 		// Use the view returned by the fragment to call findViewById
@@ -64,244 +63,30 @@ public class ButtonFragment extends Fragment {
 		//redButton.setImageDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
 		//redButton.setImageResource(WastedTimeIcon.valueOf("ICON"+icon).id);
 		
-		final ImageButton i1 = (ImageButton) rootView.findViewById(R.id.imageButton1);
-		final ImageButton i2 = (ImageButton) rootView.findViewById(R.id.imageButton2);
-		final ImageButton i3 = (ImageButton) rootView.findViewById(R.id.imageButton3);
-		final ImageButton i4 = (ImageButton) rootView.findViewById(R.id.imageButton4);
-		final ImageButton i5 = (ImageButton) rootView.findViewById(R.id.imageButton5);
-		final ImageButton i6 = (ImageButton) rootView.findViewById(R.id.imageButton6);
-		final ImageButton i7 = (ImageButton) rootView.findViewById(R.id.imageButton7);
-		final ImageButton i8 = (ImageButton) rootView.findViewById(R.id.imageButton8);
-		final ImageButton i9 = (ImageButton) rootView.findViewById(R.id.imageButton9);
-		final ImageButton i10 = (ImageButton) rootView.findViewById(R.id.imageButton10);
-		final ImageButton i11 = (ImageButton) rootView.findViewById(R.id.imageButton11);
-		final ImageButton i12 = (ImageButton) rootView.findViewById(R.id.imageButton12);
-		final ImageButton i13 = (ImageButton) rootView.findViewById(R.id.imageButton13);
-		final ImageButton i14 = (ImageButton) rootView.findViewById(R.id.imageButton14);
-		final ImageButton i15 = (ImageButton) rootView.findViewById(R.id.imageButton15);
-		final ImageButton i16 = (ImageButton) rootView.findViewById(R.id.imageButton16);
-		
-		i1.setOnClickListener(mIconListener);
-		i2.setOnClickListener(mIconListener);
-		i3.setOnClickListener(mIconListener);
-		i4.setOnClickListener(mIconListener);
-		i5.setOnClickListener(mIconListener);
-		i6.setOnClickListener(mIconListener);
-		i7.setOnClickListener(mIconListener);
-		i8.setOnClickListener(mIconListener);
-		i9.setOnClickListener(mIconListener);
-		i10.setOnClickListener(mIconListener);
-		i11.setOnClickListener(mIconListener);
-		i12.setOnClickListener(mIconListener);
-		i13.setOnClickListener(mIconListener);
-		i14.setOnClickListener(mIconListener);
-		i15.setOnClickListener(mIconListener);
-		i16.setOnClickListener(mIconListener);
+		for (WastedTimeIcon icon : WastedTimeIcon.values()){
+			ImageButton imageButton = (ImageButton) rootView.findViewById(icon.associatedBackgroundId);
+			imageButton.setOnClickListener(mIconListener);
+		}
 		
 		return rootView;
 		
 	}
 	
 	private OnClickListener mIconListener = new OnClickListener() {
-	    public void onClick(View v) {
-	    	switch(v.getId()) {
-	        case R.id.imageButton1:
-	        	icon = 1;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-		          break;
-	        case R.id.imageButton2:
-				icon = 2;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton3:
-	        	icon = 3;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton4:
-	        	icon = 4;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton5:
-	        	icon = 5;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton6:
-	        	icon = 6;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton7:
-	        	icon = 7;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton8:
-	        	icon = 8;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton9:
-	        	icon = 9;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton10:
-	        	icon = 10;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton11:
-	        	icon = 11;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton12:
-	        	icon = 12;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton13:
-	        	icon = 13;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton14:
-	        	icon = 14;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton15:
-	        	icon = 15;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	        case R.id.imageButton16:
-	        	icon = 16;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-	          break;
-	      }
+	    @SuppressWarnings("deprecation")
+		public void onClick(View v) {
+	    	WastedTimeIcon icon = null;
+	    	WastedTimeIcon[] iconList = WastedTimeIcon.values();
+	    	for (int i = 0; i < iconList.length && icon == null; i++){
+	    		if (iconList[i].associatedBackgroundId == v.getId()){
+	    			icon = iconList[i];
+	    		}
+	    	}
+	    	
+	    	redButton.setBackgroundDrawable(getResources().getDrawable(icon.id));
 	    }
 	};
 	
-	
-/*	private void onIconClick() {
-		
-		final ImageButton i2 = (ImageButton) rootView.findViewById(R.id.imageButton2);
-		i2.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 2;
-				//redButton.setImageDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-			}
-		});
-		final ImageButton i3 = (ImageButton) rootView.findViewById(R.id.imageButton3);
-		i3.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 3;
-				redButton.setBackgroundDrawable(getResources().getDrawable(WastedTimeIcon.valueOf("ICON"+icon).id));
-			}
-		});		
-		final ImageButton i4 = (ImageButton) rootView.findViewById(R.id.imageButton4);
-		i4.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 4;
-			}
-		});
-		final ImageButton i5 = (ImageButton) rootView.findViewById(R.id.imageButton5);
-		i5.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 5;
-			}
-		});
-		final ImageButton i6 = (ImageButton) rootView.findViewById(R.id.imageButton6);
-		i6.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 6;
-			}
-		});
-		final ImageButton i7 = (ImageButton) rootView.findViewById(R.id.imageButton7);
-		i7.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 7;
-			}
-		});
-		final ImageButton i8 = (ImageButton) rootView.findViewById(R.id.imageButton8);
-		i8.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 8;
-			}
-		});
-		final ImageButton i9 = (ImageButton) rootView.findViewById(R.id.imageButton9);
-		i9.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 9;
-			}
-		});
-		final ImageButton i10 = (ImageButton) rootView.findViewById(R.id.imageButton10);
-		i10.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 10;
-			}
-		});
-		final ImageButton i11 = (ImageButton) rootView.findViewById(R.id.imageButton11);
-		i11.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 11;
-			}
-		});
-		final ImageButton i12 = (ImageButton) rootView.findViewById(R.id.imageButton12);
-		i12.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 12;
-			}
-		});
-		final ImageButton i13 = (ImageButton) rootView.findViewById(R.id.imageButton13);
-		i13.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 13;
-			}
-		});
-		final ImageButton i14 = (ImageButton) rootView.findViewById(R.id.imageButton14);
-		i14.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 14;
-			}
-		});
-		final ImageButton i15 = (ImageButton) rootView.findViewById(R.id.imageButton15);
-		i15.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 15;
-			}
-		});
-		final ImageButton i16 = (ImageButton) rootView.findViewById(R.id.imageButton16);
-		i16.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				icon = 16;
-			}
-		});
-		
-	}*/
-
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == 1) {
--- WastedTime/src/com/aachaerandio/wastedtime/service/TimeBean.java
+++ WastedTime/src/com/aachaerandio/wastedtime/service/TimeBean.java
@@ -7,27 +7,30 @@ import com.aachaerandio.wastedtime.R;
 public class TimeBean {
 	
 	public enum WastedTimeIcon {
-		ICON1(R.drawable.wastedtime_icon_001), //0
-		ICON2(R.drawable.wastedtime_icon_002), //1
-		ICON3(R.drawable.wastedtime_icon_003), //2
-		ICON4(R.drawable.wastedtime_icon_004),
-		ICON5(R.drawable.wastedtime_icon_005),
-		ICON6(R.drawable.wastedtime_icon_006),
-		ICON7(R.drawable.wastedtime_icon_007),
-		ICON8(R.drawable.wastedtime_icon_008),
-		ICON9(R.drawable.wastedtime_icon_009),
-		ICON10(R.drawable.wastedtime_icon_010),
-		ICON11(R.drawable.wastedtime_icon_011),
-		ICON12(R.drawable.wastedtime_icon_012),
-		ICON13(R.drawable.wastedtime_icon_013),
-		ICON14(R.drawable.wastedtime_icon_014),
-		ICON15(R.drawable.wastedtime_icon_015),
-		ICON16(R.drawable.wastedtime_icon_016);
+		ICON1(R.drawable.wastedtime_icon_001, R.id.imageButton1), //0
+		ICON2(R.drawable.wastedtime_icon_002, R.id.imageButton2), //1
+		ICON3(R.drawable.wastedtime_icon_003, R.id.imageButton3), //2
+		ICON4(R.drawable.wastedtime_icon_004, R.id.imageButton4),
+		ICON5(R.drawable.wastedtime_icon_005, R.id.imageButton5),
+		ICON6(R.drawable.wastedtime_icon_006, R.id.imageButton6),
+		ICON7(R.drawable.wastedtime_icon_007, R.id.imageButton7),
+		ICON8(R.drawable.wastedtime_icon_008, R.id.imageButton8),
+		ICON9(R.drawable.wastedtime_icon_009, R.id.imageButton9),
+		ICON10(R.drawable.wastedtime_icon_010, R.id.imageButton10),
+		ICON11(R.drawable.wastedtime_icon_011, R.id.imageButton11),
+		ICON12(R.drawable.wastedtime_icon_012, R.id.imageButton12),
+		ICON13(R.drawable.wastedtime_icon_013, R.id.imageButton13),
+		ICON14(R.drawable.wastedtime_icon_014, R.id.imageButton14),
+		ICON15(R.drawable.wastedtime_icon_015, R.id.imageButton15),
+		ICON16(R.drawable.wastedtime_icon_016, R.id.imageButton16);
 		
 		public int id;
+		
+		public int associatedBackgroundId;
 	
-		private WastedTimeIcon(int id) {
+		private WastedTimeIcon(int id, int associatedBackgroundId) {
 			this.id = id;
+			this.associatedBackgroundId = associatedBackgroundId;
 		}
 	};
 	
